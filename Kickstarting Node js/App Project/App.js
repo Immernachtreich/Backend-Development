82494b7cffeab8fcbb262ad6d96b4503871ae224
Immernachtreich <ebrahimtohfafarosh11@gmail.com>
2022-11-04 13:12:12 +0530

    Added read & write from file features

--- Kickstarting Node js/App Project/App.js
+++ Kickstarting Node js/App Project/App.js
@@ -1,12 +1,62 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer( (request, response) => {
-    console.log(request.url, request.method, request.headers);
+    // console.log(request.url, request.method, request.headers);
     //process.exit();
 
     response.setHeader('Content-Type', 'text/html');
 
-    if(request.url == '/home') {
+    const url = request.url;
+
+    if(url === '/') {
+
+        fs.readFile('message.txt', 'utf-8',(err, data) => {
+            
+            response.write(
+                `<html>
+                <head>
+                    <title> Enter Message </title>
+                </head>
+                <body>
+                    <p> ${data} <p>
+                    <form action="/message" method="POST">
+                        <input type="text" name="message">
+                        <button type="submit"> Send </button>
+                    </form>
+                </body>
+                </html>`);
+    
+            return response.end();
+        })
+    } 
+
+    if(url === '/message' && request.method === 'POST') {
+
+        const body = [];
+
+        // Oncoming data request
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        })
+
+        // On end of data stream
+        request.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+
+            fs.writeFile('message.txt', message, () => {
+
+                // Setting status code and location back to '/'
+                response.statusCode = 302;
+                response.setHeader('Location', '/');
+                
+                return response.end();
+            });
+        })
+    }
+
+    if(url === '/home') {
 
         response.write(
             `<html>
@@ -18,7 +68,11 @@ const server = http.createServer( (request, response) => {
             </body>
             </html>`);
 
-    } else if(request.url == '/about') {
+        return response.end();
+
+    }
+    
+    if(url === '/about') {
 
         response.write(
             `<html>
@@ -30,7 +84,11 @@ const server = http.createServer( (request, response) => {
             </body>
             </html>`);
 
-    } else if(request.url == '/node') {
+        return response.end();
+
+    }
+
+    if(url === '/node') {
 
         response.write(
             `<html>
@@ -41,6 +99,8 @@ const server = http.createServer( (request, response) => {
                 <h1> Welcome to my Node Js project </h1>
             </body>
             </html>`);
+
+        return response.end();
     }
 })
 
