d403edc9e016d4d680cbcfa54c327c693f864cae
Immernachtreich <ebrahimtohfafarosh11@gmail.com>
2022-11-05 15:27:00 +0530

    Added Body Parser and worked with req & res

--- Kickstarting Node js/App Project/App.js
+++ Kickstarting Node js/App Project/App.js
@@ -1,19 +1,38 @@
-const http = require('http'); // Node Imports
-
 const express = require('express'); // Express Imports
+const bodyParser = require('body-parser') // Body Parser Imports
 
 const app = express(); // express() is a function and calling it returns request handler
 
+
 // use() allows us to use middleware
-app.use( (request, response, next) => {
 
-    console.log('In the middleware');
-    next(); // next() allows the request to continue onto the next middleware in line
-}) 
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use( (request, response, next) => {
-    console.log('In the second middleware');
+app.use('/add-product', (request, response, next) => {
+    response.send(
+        `<html>
+        <head>
+            <title> Enter Message </title>
+        </head>
+        <body>
+            <form action="/product" method="POST">
+                <label> Product </label>
+                <input type="text" name="product">
+                <label> Size </label>
+                <input type="number" name="size">
+                <button type="submit"> Add Product </button>
+            </form>
+        </body>
+        </html>`);
+}); 
+
+app.post('/product', (request, response, next) => {
+    console.log(request.body);
+    response.redirect('/add-product');
+});
+
+app.use('/', (request, response, next) => {
     response.send(`<h1> Hello from Express </h1>`);
-}) 
+});
 
 app.listen(3000);
